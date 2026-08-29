b06ac2a1114c46ed1399662082c5c10aaf6329ba
theflex5710 <devrimcavusoglu34@gmail.com>
2026-08-29 10:47:45 +0300

    All Files

---
+++ Elytra Mace Addon/src/main/java/com/autism/elytramace/ElytraMaceInit.java
@@ -0,0 +1,8 @@
+package com.autism.elytramace;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ElytraMaceInit implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
